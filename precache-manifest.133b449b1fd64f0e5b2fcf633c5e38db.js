53d9dd6e2978dee7caabefb7d2a1fa3025bed16d
dzemenu <dilu.kefale@gmail.com>
2020-09-17 09:53:49 +0300

    Updates

---
+++ precache-manifest.133b449b1fd64f0e5b2fcf633c5e38db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93cdcdbe34ff943ccdd5e2e60e3823b",
+    "url": "/dzemenu/index.html"
+  },
+  {
+    "revision": "27319a852348871239c9",
+    "url": "/dzemenu/static/css/main.9fdb1473.chunk.css"
+  },
+  {
+    "revision": "78503ccbf88ce9be46b0",
+    "url": "/dzemenu/static/js/2.796c737f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dzemenu/static/js/2.796c737f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27319a852348871239c9",
+    "url": "/dzemenu/static/js/main.3a4ba0c7.chunk.js"
+  },
+  {
+    "revision": "f41affcda2810bd0cfd5",
+    "url": "/dzemenu/static/js/runtime-main.a3ca18e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dzemenu/precache-manifest.65693c448f38ce83bad864dbab618ad0.js"
+  "/dzemenu/precache-manifest.133b449b1fd64f0e5b2fcf633c5e38db.js"
 );
 
 self.addEventListener('message', (event) => {
